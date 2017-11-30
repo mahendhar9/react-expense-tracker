34c69286add047e8ca1e8f1f7be9ce5b39517313
Mahendhar <mahendarreddy@live.com>
2017-11-30 22:09:20 +0530

    Remove redux dev tools

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import AppRouter from './routes';
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducers);
 
 const jsx = (
   <Provider store={store}>
